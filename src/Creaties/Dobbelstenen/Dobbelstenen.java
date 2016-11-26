7ca6d8ac382ae329c9fdbb9411581e094c6fe688
thomkok18 <tkok@roc-dev.com>
2016-11-26 16:53:23 +0100

    update

--- src/Creaties/Dobbelstenen/Dobbelstenen.java
+++ src/Creaties/Dobbelstenen/Dobbelstenen.java
@@ -5,19 +5,27 @@ package Creaties.Dobbelstenen;
 import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
+import java.net.*;
 
 // Een klasse met de naam Opdracht1 van het type Applet.
 @SuppressWarnings("serial")
 public class Dobbelstenen extends Applet {
-    String[] cijfers = {"Aas", "Twee", "Drie", "Vier", "Vijf", "Zes", "Zeven", "Acht", "Negen", "Tien", "Boer", "Koningin", "Koning", "Joker"};
-    String[] kleuren = {"Ruiten", "Klaver", "Harten", "Schoppen"};
+    String[] dobbelsteen;
     double random;
     int worp;
+    private Image afbeelding;
+    private URL pad;
     Button gooiKnop;
 
     // Een (lege) methode die de Applet gaat initialiseren.
     public void init() {
         setSize(400, 200);
+        dobbelsteen = new String[6];
+        for (int i = 0; i < dobbelsteen.length; i++) {
+
+        }
+
+        afbeelding = getImage(pad, "dobbel1.jpg");
         gooiKnop = new Button("Gooi");
         gooiKnop.addActionListener(new DobbelKnopLuisteraar());
         add(gooiKnop);
@@ -26,13 +34,14 @@ public class Dobbelstenen extends Applet {
     class DobbelKnopLuisteraar implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             random = Math.random();
-            worp = (int) (random * 6);
+            worp = (int) (random * 6 + 1);
             repaint();
         }
     }
 
     // Een methode die de inhoud van het scherm tekent.
     public void paint(Graphics g) {
-        g.drawString("" + worp, 50, 50);
+        g.drawString(pad.toString(), 20, 10 );
+        g.drawImage(afbeelding, 20, 20, 400, 300, this);
     }
 }
---
+++ src/Opdrachten/H12/Opdracht2.java
@@ -0,0 +1,24 @@
+// Hoofdstuk 12
+package Opdrachten.H12;
+
+//Importeer de benodigde klassen uit de Java bibliotheek.
+import java.awt.*;
+import java.applet.*;
+
+// Een klasse met de naam Opdracht2 van het type Applet.
+@SuppressWarnings("serial")
+public class Opdracht2 extends Applet {
+	int smileyKnoppen[];
+	Button smileyKnop;
+
+	// Een (lege) methode die de Applet gaat initialiseren.
+	public void init() {
+		setSize(400, 400);
+		smileyKnoppen = new int[25];
+		for (int smiley = 0; smiley < smileyKnoppen.length; smiley++) {
+			smileyKnoppen[smiley] = smiley;
+			smileyKnop = new Button("=D");
+			add(smileyKnop);
+		}
+	}
+}
