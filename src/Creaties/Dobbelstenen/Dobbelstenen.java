821e394831573f1ce85111f17a01b3be4edb050e
thomkok18 <tkok@roc-dev.com>
2016-11-26 21:07:06 +0100

    update

--- src/Creaties/Dobbelstenen/Dobbelstenen.java
+++ src/Creaties/Dobbelstenen/Dobbelstenen.java
@@ -10,7 +10,6 @@ import java.net.*;
 // Een klasse met de naam Opdracht1 van het type Applet.
 @SuppressWarnings("serial")
 public class Dobbelstenen extends Applet {
-    String[] dobbelsteen;
     double random;
     int worp;
     private Image afbeelding;
@@ -19,28 +18,25 @@ public class Dobbelstenen extends Applet {
 
     // Een (lege) methode die de Applet gaat initialiseren.
     public void init() {
-        setSize(400, 200);
-        dobbelsteen = new String[6];
-        for (int i = 0; i < dobbelsteen.length; i++) {
-            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelsteen/dobbel" + i + ".jpg");
-            afbeelding = getImage(pad, "dobbel"+ i +".jpg");
-        }
-//        gooiKnop = new Button("Gooi");
-//        gooiKnop.addActionListener(new DobbelKnopLuisteraar());
-//        add(gooiKnop);
+        setSize(400, 400);
+        gooiKnop = new Button("Gooi");
+        gooiKnop.addActionListener(new DobbelKnopLuisteraar());
+        add(gooiKnop);
     }
 
-//    class DobbelKnopLuisteraar implements ActionListener {
-//        public void actionPerformed(ActionEvent e) {
-//            random = Math.random();
-//            worp = (int) (random * 6 + 1);
-//            repaint();
-//        }
-//    }
+    class DobbelKnopLuisteraar implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            random = Math.random();
+            worp = (int) (random * 6);
+                pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Wit/dobbel" + worp + ".jpg");
+                afbeelding = getImage(pad, "dobbel"+ worp +".jpg");
+
+            repaint();
+        }
+    }
 
     // Een methode die de inhoud van het scherm tekent.
     public void paint(Graphics g) {
-        g.drawString(pad.toString(), 20, 10 );
-        g.drawImage(afbeelding, 20, 20, 400, 300, this);
+        g.drawImage(afbeelding, 40, 40, 50, 50, this);
     }
 }
