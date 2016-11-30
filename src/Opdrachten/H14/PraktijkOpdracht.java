e97e531a68e2ff6b8afad0276800c825a3ad2ceb
thomkok18 <tkok@roc-dev.com>
2016-11-30 13:46:18 +0100

    update

--- src/Opdrachten/H14/PraktijkOpdracht.java
+++ src/Opdrachten/H14/PraktijkOpdracht.java
@@ -12,14 +12,26 @@ import java.net.*;
 public class PraktijkOpdracht extends Applet {
     private Image smiley;
     private URL pad;
-    String[] smileyArray;
-    int aantalRijen, aantalSmileys, x, y;
+    Label label;
+    TextField tekstvak;
+    double random;
+    int aantalRijen, aantalSmileys, computerGetal, winnendGetal, x, y;
     Button SpeelKnop;
+    boolean foutmelding, geklikt;
+    String error;
 
     // Een (lege) methode die de Applet gaat initialiseren.
     public void init() {
         setSize(600, 400);
-        smileyArray = new String[23];
+        foutmelding = false;
+        geklikt = false;
+        aantalSmileys = 23;
+        aantalRijen = 6;
+        error = "";
+        label = new Label("Hoeveel smileys neem je (één, twee of drie)?");
+        add(label);
+        tekstvak = new TextField("", 3);
+        add(tekstvak);
         SpeelKnop = new Button("Speel");
         SpeelKnop.addActionListener(new SpeelKnopLuisteraar());
         add(SpeelKnop);
@@ -29,26 +41,86 @@ public class PraktijkOpdracht extends Applet {
 
     class SpeelKnopLuisteraar implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            repaint();
+            try {
+                // Speler is aan zet
+                error = "";
+                int getal = Integer.parseInt(tekstvak.getText());
+                if (getal == 1 || getal == 2 || getal == 3) {
+                    aantalSmileys = aantalSmileys - getal;
+
+                    if (aantalSmileys % 4 == 0 + 1) {
+                        random = Math.random();
+                        computerGetal = (int) (random * 3);
+                        aantalSmileys = aantalSmileys - computerGetal;
+                        pad = PraktijkOpdracht.class.getResource("/Opdrachten/H14/Smileys/Smiley-Blij.png");
+                        smiley = getImage(pad, "Smiley-Blij.png");
+                    } else {
+                        pad = PraktijkOpdracht.class.getResource("/Opdrachten/H14/Smileys/Smiley-Neutraal.png");
+                        smiley = getImage(pad, "Smiley-Neutraal.png");
+                    }
+                    // Computer is aan zet
+                    if (aantalSmileys == 2 || aantalSmileys == 6 || aantalSmileys == 10 || aantalSmileys == 14 || aantalSmileys == 18 || aantalSmileys == 22) {
+                        computerGetal = 1;
+                        aantalSmileys = aantalSmileys - computerGetal;
+                    }
+                    if (aantalSmileys == 3 || aantalSmileys == 7 || aantalSmileys == 11 || aantalSmileys == 15 || aantalSmileys == 19) {
+                        computerGetal = 2;
+                        aantalSmileys = aantalSmileys - computerGetal;
+                    }
+                    if (aantalSmileys == 4 || aantalSmileys == 8 || aantalSmileys == 12 || aantalSmileys == 16 || aantalSmileys == 20) {
+                        computerGetal = 3;
+                        aantalSmileys = aantalSmileys - computerGetal;
+                    }
+
+                    geklikt = true;
+                    repaint();
+                } else {
+                    foutmelding = true;
+                    geklikt = true;
+                    error = "De variable klopt niet.";
+                    tekstvak.setText("");
+                    repaint();
+                }
+                } catch(NumberFormatException exc){
+                    foutmelding = true;
+                    geklikt = true;
+                    error = "De variable klopt niet.";
+                    tekstvak.setText("");
+                    repaint();
+                }
+            }
         }
-    }
 
     // Een methode die de inhoud van het scherm tekent.
     public void paint(Graphics g) {
         aantalRijen = 6;
-        aantalSmileys = 4;
-        x = 20;
-        y = 0;
-        for (int rijen = 0; rijen < aantalRijen; rijen++) {
-            y += 50;
-            x = 20;
-            if (rijen == 5) {
-                aantalSmileys = 3;
+        x = 50;
+        y = 40;
+        if (foutmelding == false) {
+            if (geklikt == true) {
+                g.drawString("De computer heeft " + computerGetal + " smileys weggehaald", 50, 50);
             }
-            for (int smileys = 0; smileys < aantalSmileys; smileys++) {
+            g.drawString("Aantal smileys: " + aantalSmileys + " . Jouw beurt.", 50, 70);
+            for (int smileys = 0; smileys < (aantalSmileys); smileys++) {
+                if (smileys % 4 == 0) {
+                    x = 50;
+                    y += 50;
+                }
                 g.drawImage(smiley, x, y, 40, 40, this);
                 x += 50;
             }
+        } else {
+            g.drawString("" + error, 50, 50);
+            for (int smileys = 0; smileys < (aantalSmileys); smileys++) {
+                if (smileys % 4 == 0) {
+                    x = 50;
+                    y += 50;
+                }
+                g.drawImage(smiley, x, y, 40, 40, this);
+                x += 50;
+            }
+            foutmelding = false;
         }
+        tekstvak.setText("");
     }
 }
