7bb3d7da587155f9c13f65560a22bb41507fba02
thomkok18 <tkok@roc-dev.com>
2016-12-16 09:58:36 +0100

    update

--- src/Creaties/Dobbelstenen/Dobbelstenen.java
+++ src/Creaties/Dobbelstenen/Dobbelstenen.java
@@ -27,52 +27,52 @@ public class Dobbelstenen extends Applet {
     class GooiKnopLuisteraar implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             double randomWit = Math.random();
-            int worpWit = (int) (randomWit * 6);
+            int worpWit = (int) (randomWit * 5 + 1);
             pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Wit/dobbel" + worpWit + ".jpg");
             afbeeldingWit = getImage(pad, "dobbel"+ worpWit +".jpg");
 
             double randomBlauw = Math.random();
-            int worpBlauw = (int) (randomBlauw * 6);
+            int worpBlauw = (int) (randomBlauw * 5 + 1);
             pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Blauw/dobbel" + worpBlauw + ".jpg");
             afbeeldingBlauw = getImage(pad, "dobbel"+ worpBlauw +".jpg");
 
             double randomGroen = Math.random();
-            int worpGroen = (int) (randomGroen * 6);
+            int worpGroen = (int) (randomGroen * 5 + 1);
             pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Groen/dobbel" + worpGroen + ".jpg");
             afbeeldingGroen = getImage(pad, "dobbel"+ worpGroen +".jpg");
 
             double randomRood = Math.random();
-            int worpRood = (int) (randomRood * 6);
+            int worpRood = (int) (randomRood * 5 + 1);
             pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Rood/dobbel" + worpRood + ".jpg");
             afbeeldingRood = getImage(pad, "dobbel"+ worpRood +".jpg");
 
             double randomGeel = Math.random();
-            int worpGeel = (int) (randomGeel * 6);
+            int worpGeel = (int) (randomGeel * 5 + 1);
             pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Geel/dobbel" + worpGeel + ".jpg");
             afbeeldingGeel = getImage(pad, "dobbel"+ worpGeel +".jpg");
 
             double randomOranje = Math.random();
-            int worpOranje = (int) (randomOranje * 6);
+            int worpOranje = (int) (randomOranje * 5 + 1);
             pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Oranje/dobbel" + worpOranje + ".jpg");
             afbeeldingOranje = getImage(pad, "dobbel"+ worpOranje +".jpg");
 
             double randomBruin = Math.random();
-            int worpBruin = (int) (randomBruin * 6);
+            int worpBruin = (int) (randomBruin * 5 + 1);
             pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Bruin/dobbel" + worpBruin + ".jpg");
             afbeeldingBruin = getImage(pad, "dobbel"+ worpBruin +".jpg");
 
             double randomPaars = Math.random();
-            int worpPaars = (int) (randomPaars * 6);
+            int worpPaars = (int) (randomPaars * 5 + 1);
             pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Paars/dobbel" + worpPaars + ".jpg");
             afbeeldingPaars = getImage(pad, "dobbel"+ worpPaars +".jpg");
 
             double randomRoze = Math.random();
-            int worpRoze = (int) (randomRoze * 6);
+            int worpRoze = (int) (randomRoze * 5 + 1);
             pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Roze/dobbel" + worpRoze + ".jpg");
             afbeeldingRoze = getImage(pad, "dobbel"+ worpRoze +".jpg");
 
             double randomGrijs = Math.random();
-            int worpGrijs = (int) (randomGrijs * 6);
+            int worpGrijs = (int) (randomGrijs * 5 + 1);
             pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Grijs/dobbel" + worpGrijs + ".jpg");
             afbeeldingGrijs = getImage(pad, "dobbel"+ worpGrijs +".jpg");
 
--- src/Opdrachten/H14/Toets2.java
+++ src/Opdrachten/H14/Toets2.java
@@ -4,41 +4,105 @@ package Opdrachten.H14;
 // Importeer de benodigde klassen uit de Java bibliotheek.
 
 import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.net.URL;
 
 // Een klasse met de naam Toets1 van het type Applet.
 @SuppressWarnings("serial")
 public class Toets2 extends Applet {
 	Button speelKnop, koopKnop;
+	int krediet, punten;
+	private Image afbeeldingFruit1, afbeeldingFruit2, afbeeldingFruit3;
+	private URL pad;
+	private AudioClip win, jackpot;
+	boolean gewonnen;
 
 	// Een (lege) methode die de Applet gaat initialiseren.
 	public void init() {
 		setSize(500, 400);
+		krediet = 10;
+		punten = 0;
+		gewonnen = false;
+
+		pad = Toets2.class.getResource("/Opdrachten/H14/Audio/Fruitmachine/jackpot.wav");
+		jackpot = getAudioClip(pad, "jackpot.wav");
+
+		pad = Toets2.class.getResource("/Opdrachten/H14/Audio/Fruitmachine/slot-payoff.wav");
+		win = getAudioClip(pad, "slot-payoff.wav");
 
 		speelKnop = new Button("Speel");
 		speelKnop.addActionListener( new SpeelKnopListener() );
 		add(speelKnop);
 
 		koopKnop = new Button("Koop 10 kredietpunten");
-		koopKnop.addActionListener( new WijnListener() );
+		koopKnop.addActionListener( new KoopKnopListener() );
 		add(koopKnop);
+		koopKnop.setEnabled(false);
 	}
 
 	// Een methode die de inhoud van het scherm tekent.	
 	public void paint(Graphics g) {
-
+		g.drawString("Krediet: " + krediet, 50, 270);
+		if (gewonnen == true) {
+			g.drawString("Gewonnen: " + punten + " punten!", 50, 290);
+		}
+		g.drawImage(afbeeldingFruit1, 50, 50, 100, 200, this);
+		g.drawImage(afbeeldingFruit2, 150, 50, 100, 200, this);
+		g.drawImage(afbeeldingFruit3, 250, 50, 100, 200, this);
 	}
 
 	class SpeelKnopListener implements ActionListener	{
 		public void actionPerformed( ActionEvent e ) {
+			gewonnen = false;
+			punten = 0;
+
+			double randomFruit1 = Math.random();
+			int speel1 = (int) (randomFruit1 * 20 + 1);
+			pad = Toets2.class.getResource("/Opdrachten/H14/img/Fruitplaatjes/fruit_" + speel1 + ".jpg");
+			afbeeldingFruit1 = getImage(pad, "fruit_"+ speel1 +".jpg");
+
+			double randomFruit2 = Math.random();
+			int speel2 = (int) (randomFruit2 * 20 + 1);
+			pad = Toets2.class.getResource("/Opdrachten/H14/img/Fruitplaatjes/fruit_" + speel2 + ".jpg");
+			afbeeldingFruit2 = getImage(pad, "fruit_"+ speel2 +".jpg");
+
+			double randomFruit3 = Math.random();
+			int speel3 = (int) (randomFruit3 * 20 + 1);
+			pad = Toets2.class.getResource("/Opdrachten/H14/img/Fruitplaatjes/fruit_" + speel3 + ".jpg");
+			afbeeldingFruit3 = getImage(pad, "fruit_"+ speel3 +".jpg");
+
+			if (afbeeldingFruit1 == afbeeldingFruit2 && afbeeldingFruit2 == afbeeldingFruit3) {
+				punten = 20;
+				krediet += 20;
+				gewonnen = true;
+				win.play();
+			}
+			if (afbeeldingFruit1 == afbeeldingFruit2 || afbeeldingFruit1 == afbeeldingFruit3 || afbeeldingFruit2 == afbeeldingFruit3) {
+				punten = 4;
+				krediet += 4;
+				gewonnen = true;
+				jackpot.play();
+			}
+			if (krediet > 1) {
+				krediet--;
+			} else {
+				krediet = 0;
+				speelKnop.setEnabled(false);
+				koopKnop.setEnabled(true);
+			}
 			repaint();
 		}
 	}
 
-	class WijnListener implements ActionListener	{
+	class KoopKnopListener implements ActionListener	{
 		public void actionPerformed( ActionEvent e ) {
+			if (krediet == 0) {
+				krediet += 10;
+				speelKnop.setEnabled(true);
+				koopKnop.setEnabled(false);
+			}
 			repaint();
 		}
 	}
