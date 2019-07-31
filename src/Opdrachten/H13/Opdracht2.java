a0337b3327e60e49290c63cf21630e661384256b
Thomkok <thomkok13@hotmail.com>
2019-07-31 23:44:37 +0200

    Update

--- src/Opdrachten/H13/Opdracht2.java
+++ src/Opdrachten/H13/Opdracht2.java
@@ -14,7 +14,7 @@ public class Opdracht2 extends Applet {
 		setSize(400, 200);
 	}
 
-	public 	void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
+	public void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
 		g.drawLine(x1, y1, x2, y2);
 		g.drawLine(x3, y3, x1, y1);
 		g.drawLine(x2, y2, x3, y3);
