a6c397e8d1f8906c8e84a5385f7d5806d50dbb77
Thomkok <thomkok13@hotmail.com>
2019-08-01 14:38:20 +0200

    Update

--- src/Creaties/Dobbelstenen/Dobbelstenen.java
+++ src/Creaties/Dobbelstenen/Dobbelstenen.java
@@ -2,7 +2,6 @@
 package Creaties.Dobbelstenen;
 
 // Importeer de benodigde klassen uit de Java bibliotheek.
-import javafx.scene.control.ComboBox;
 
 import java.awt.*;
 import java.applet.*;
@@ -28,52 +27,52 @@ public class Dobbelstenen extends Applet {
         public void actionPerformed(ActionEvent e) {
             double randomWit = Math.random();
             int worpWit = (int) (randomWit * 5);
-            pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Wit/dobbel" + worpWit + ".jpg");
+            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelstenen/img/Wit/dobbel" + worpWit + ".jpg");
             afbeeldingWit = getImage(pad);
 
             double randomBlauw = Math.random();
             int worpBlauw = (int) (randomBlauw * 6);
-            pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Blauw/dobbel" + worpBlauw + ".jpg");
+            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelstenen/img/Blauw/dobbel" + worpBlauw + ".jpg");
             afbeeldingBlauw = getImage(pad);
 
             double randomGroen = Math.random();
             int worpGroen = (int) (randomGroen * 6);
-            pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Groen/dobbel" + worpGroen + ".jpg");
+            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelstenen/img/Groen/dobbel" + worpGroen + ".jpg");
             afbeeldingGroen = getImage(pad);
 
             double randomRood = Math.random();
             int worpRood = (int) (randomRood * 6);
-            pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Rood/dobbel" + worpRood + ".jpg");
+            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelstenen/img/Rood/dobbel" + worpRood + ".jpg");
             afbeeldingRood = getImage(pad);
 
             double randomGeel = Math.random();
             int worpGeel = (int) (randomGeel * 6);
-            pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Geel/dobbel" + worpGeel + ".jpg");
+            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelstenen/img/Geel/dobbel" + worpGeel + ".jpg");
             afbeeldingGeel = getImage(pad);
 
             double randomOranje = Math.random();
             int worpOranje = (int) (randomOranje * 6);
-            pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Oranje/dobbel" + worpOranje + ".jpg");
+            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelstenen/img/Oranje/dobbel" + worpOranje + ".jpg");
             afbeeldingOranje = getImage(pad);
 
             double randomBruin = Math.random();
             int worpBruin = (int) (randomBruin * 6);
-            pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Bruin/dobbel" + worpBruin + ".jpg");
+            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelstenen/img/Bruin/dobbel" + worpBruin + ".jpg");
             afbeeldingBruin = getImage(pad);
 
             double randomPaars = Math.random();
             int worpPaars = (int) (randomPaars * 6);
-            pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Paars/dobbel" + worpPaars + ".jpg");
+            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelstenen/img/Paars/dobbel" + worpPaars + ".jpg");
             afbeeldingPaars = getImage(pad);
 
             double randomRoze = Math.random();
             int worpRoze = (int) (randomRoze * 6);
-            pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Roze/dobbel" + worpRoze + ".jpg");
+            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelstenen/img/Roze/dobbel" + worpRoze + ".jpg");
             afbeeldingRoze = getImage(pad);
 
             double randomGrijs = Math.random();
             int worpGrijs = (int) (randomGrijs * 6);
-            pad = Dobbelstenen.class.getResource("/Creaties/KleurDobbelstenen/Grijs/dobbel" + worpGrijs + ".jpg");
+            pad = Dobbelstenen.class.getResource("/Creaties/Dobbelstenen/img/Grijs/dobbel" + worpGrijs + ".jpg");
             afbeeldingGrijs = getImage(pad);
             repaint();
         }
@@ -82,15 +81,15 @@ public class Dobbelstenen extends Applet {
     // Een methode die de inhoud van het scherm tekent.
     public void paint(Graphics g) {
         int x = 40;
-            g.drawImage(afbeeldingWit, x, 40, 50, 50, this);
+        g.drawImage(afbeeldingWit, x, 40, 50, 50, this);
         x += 80;
-            g.drawImage(afbeeldingBlauw, x, 40, 50, 50, this);
+        g.drawImage(afbeeldingBlauw, x, 40, 50, 50, this);
         x += 80;
-            g.drawImage(afbeeldingGroen, x, 40, 50, 50, this);
+        g.drawImage(afbeeldingGroen, x, 40, 50, 50, this);
         x += 80;
-            g.drawImage(afbeeldingRood, x, 40, 50, 50, this);
+        g.drawImage(afbeeldingRood, x, 40, 50, 50, this);
         x += 80;
-            g.drawImage(afbeeldingGeel, x, 40, 50, 50, this);
+        g.drawImage(afbeeldingGeel, x, 40, 50, 50, this);
         x = 40;
         g.drawImage(afbeeldingOranje, x, 120, 50, 50, this);
         x += 80;
